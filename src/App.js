c1f0b8ad6d9725ba79f3e776416fffdcf083db1e
Ruan Silva <ruan.silva@widesoftware.com.br>
2019-10-31 16:00:58 -0300

    Mão na massa comecando projeto

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+import './global.css';
+import Header from './components/Header';
+
+
+class App extends Component {
+    render(){
+        return(
+            <BrowserRouter>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../global.css';
+
+class Home extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Tela Home</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
